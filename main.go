0df1656902fa29ef43a951a53a061b1655ceef18
eghbali.ehsan <eghbali.ehsan@sabaidea.team>
2025-02-25 19:22:22 +0330

    section 3

--- main.go
+++ main.go
@@ -2,31 +2,55 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	"sync"
 	"time"
 )
 
-func main() {
-	a := runtime.GOMAXPROCS(2)
-	go helloName("ehsan")
-	fmt.Println("Hello World")
-	time.Sleep(1 * time.Second)
-	fmt.Println(a)
-	//lion := new(port.Lion)
-	//lion.Name = "Lion"
-	//lion.Age = 12
-	//
-	//cat := port.Cat{
-	//	Name: "Cat",
-	//}
-	//fmt.Println(lion)
-	//fmt.Println(&cat)
-	//fmt.Println(GetNameAnimal(lion))
+type calculator struct {
+	*sync.WaitGroup
+	count int
 }
-func helloName(name string) {
-	fmt.Println("Hello " + name)
+type result struct {
+	*sync.WaitGroup
+	number int
 }
 
-//func GetNameAnimal(animal port.Animal) string {
-//	return animal.GetName()
-//}
+func main() {
+	rw := new(sync.WaitGroup)
+	nc := &calculator{
+		rw,
+		0,
+	}
+	rw1 := new(sync.WaitGroup)
+	r := &result{
+		rw1,
+		1,
+	}
+	for i := 0; i < 100000; i++ {
+		nc.Add(1)
+
+		go nc.CalAdd()
+
+		for j := 0; j < 10; j++ {
+			rw1.Add(1)
+			go r.multiply(i)
+			rw1.Wait()
+		}
+		nc.Wait()
+	}
+	nc.Wait()
+	time.Sleep(5 * time.Second)
+	fmt.Println(nc.count)
+
+}
+
+func (nc *calculator) CalAdd() {
+	defer nc.Done()
+	fmt.Println(nc.count)
+	nc.count++
+}
+
+func (r *result) multiply(int2 int) {
+	defer r.Done()
+	fmt.Println(int2 * r.number)
+}
